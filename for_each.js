82bde3cb6fef1acc9ab7cac2b52ca43a113b2e68
jordy <jsmarquezb20@gmail.com>
2024-04-16 11:47:34 +0200

    for each hecho (completo)

--- for_each.js
+++ for_each.js
@@ -1,8 +1,9 @@
 const arrayNums = [10, 77, 16, 3, 20, 96, 33, 65, 82, 45];
 let suma = 0;
+let numPares = 0;
 
 // recorrer el array y mostrar el doble de cada elemento
-console.log("Elementos del array (x2): ");
+console.log("Elementos del array: ");
 arrayNums.forEach(num => console.log("Original: " + num + " - (x2): " + num*2));
 
 // funcion sumar
@@ -10,7 +11,16 @@ function sumar(numero) {
     suma += numero;
 }
 
-// recorrer el array y llamar a la funcion sumar
+// funcion contar pares
+function contarPares(numero) {
+    if (numero%2 == 0) {
+        numPares++;
+    }
+}
+
+// recorrer el array, llamar a la funcion sumar y contarPares
 arrayNums.forEach(sumar);
+arrayNums.forEach(contarPares);
 
 console.log("Suma de todos los elementos del array: " + suma);
+console.log("Cantidad de números pares en el array: " + numPares);
